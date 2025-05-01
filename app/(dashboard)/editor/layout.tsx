df007c058d85fc3f840ea5f3b919eb6ff16f8fe8
Ujjwal Prajapati <ujjwalprajapati154@gmail.com>
2025-05-01 14:44:19 +0530

    feat: add FileOpen component for displaying the currently opened file in the editor layout

--- app/(dashboard)/editor/layout.tsx
+++ app/(dashboard)/editor/layout.tsx
@@ -1,6 +1,7 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
 import EditorHeader from "./_component/EditorHeader";
 import EditorSidebar from "./_component/EditorSidebar";
+import FileOpen from "./_component/FileOpen";
 
 export default function EditorLayout({ children }: { children: React.ReactNode }) {
     return (
@@ -10,7 +11,8 @@ export default function EditorLayout({ children }: { children: React.ReactNode }
                 {/* Editor sidebar */}
                 <EditorSidebar />
                 {/* editor and file open */}
-                <main>
+                <main className="bg-gray-100 w-full">
+                    <FileOpen />
                     {children}
                 </main>
             </SidebarProvider>
